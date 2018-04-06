2d201a9259be95675f750868277f9ac5a25efe0a
Johanna Latt <j.latt@sap.com>
2018-04-06 15:09:33 -0400

    basic version with working messaging between server and mirror

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import pika
 import socket
+from enum import Enum
 
 app = Flask(__name__)
 
@@ -14,10 +15,17 @@ channel = connection.channel()
 channel.exchange_declare(exchange='mirror', exchange_type='direct')
 
 
+class MIRROR_KEY(Enum):
+    MIRROR_READY = 1
+    MIRROR_TRACKING_STARTED = 2
+    MIRROR_TRACKING_DATA = 3
+    MIRROR_TRACKING_LOST = 4
+
+
 @app.route("/")
 def hello():
     channel.basic_publish(exchange='mirror',
-                        routing_key='mirror-ready',
+                        routing_key=MIRROR_KEY.MIRROR_READY.name,
                         body='')
     return "Hello World!"
 
@@ -25,6 +33,9 @@ def hello():
 @app.route("/Tracking")
 def tracking():
     channel.basic_publish(exchange='mirror',
-                      routing_key='mirror-tracking',
+                      routing_key=MIRROR_KEY.MIRROR_TRACKING_DATA.name,
                       body='2 21 123 23 1231 346 6340 0304 23 2')
     return "It is tracking something"
+
+
+app.run(host='0.0.0.0', port=5000, debug=False)
