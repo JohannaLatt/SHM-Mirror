c07e4f94a541e3c82935bf9b4c63aba433ad7a25
Johanna Latt <j.latt@sap.com>
2018-04-09 10:42:34 -0400

    some refactoring of the mirror-code

--- index.py
+++ index.py
@@ -1,64 +1,24 @@
 from flask import Flask
-import pika
 import _thread
-from enum import Enum
+import messaging as Messaging
+from messaging import MSG_FROM_MIRROR_KEYS
+
 
 app = Flask(__name__)
 
-class MSG_FROM_MIRROR_KEYS(Enum):
-    MIRROR_READY = 1
-    MIRROR_TRACKING_STARTED = 2
-    MIRROR_TRACKING_DATA = 3
-    MIRROR_TRACKING_LOST = 4
-
-
-class MSG_TO_MIRROR_KEYS(Enum):
-    TEXT = 1
-
-
-# Create a local messaging connection
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-
-# Create an exchange for the mirror-messages - type is direct so we can distinguish the different messages
-channel.exchange_declare(exchange='from-mirror', exchange_type='direct')
-channel.exchange_declare(exchange='to-mirror', exchange_type='direct')
-
-# Declare a queue to be used (random name will be used)
-result = channel.queue_declare(exclusive=True)
-queue_name = result.method.queue
-
-# Callback for consuming incoming messages
-def consume_server_message(ch, method, properties, body):
-    print(" [x] %r:%r" % (method.routing_key, body))
-
-# Listen to server messages
-for msg_keys in MSG_TO_MIRROR_KEYS:
-    channel.queue_bind(exchange='to-mirror',
-                       queue=queue_name,
-                       routing_key=msg_keys.name)
-
-channel.basic_consume(consume_server_message,
-                      queue=queue_name,
-                      no_ack=True)
-
-_thread.start_new_thread(channel.start_consuming, ())
+Messaging.init()
+_thread.start_new_thread(Messaging.start_consuming, ())
 
 
 @app.route("/")
 def hello():
-    channel.basic_publish(exchange='from-mirror',
-                        routing_key=MSG_FROM_MIRROR_KEYS.MIRROR_READY.name,
-                        body='')
+    Messaging.send(MSG_FROM_MIRROR_KEYS.MIRROR_READY.name, "")
     return "Hello World!"
 
 
 @app.route("/tracking")
 def tracking():
-    channel.basic_publish(exchange='from-mirror',
-                      routing_key=MSG_FROM_MIRROR_KEYS.MIRROR_TRACKING_DATA.name,
-                      body='2 21 123 23 1231 346 6340 0304 23 2')
+    Messaging.send(MSG_FROM_MIRROR_KEYS.MIRROR_TRACKING_DATA.name, "2 34 23 194 93834 1828 02")
     return "It is tracking something"
-
 
 app.run(host='0.0.0.0', port=5000, debug=False)
---
+++ messaging.py
@@ -0,0 +1,63 @@
+import pika
+from enum import Enum
+import configparser
+
+
+class MSG_FROM_MIRROR_KEYS(Enum):
+    MIRROR_READY = 1
+    MIRROR_TRACKING_STARTED = 2
+    MIRROR_TRACKING_DATA = 3
+    MIRROR_TRACKING_LOST = 4
+
+
+class MSG_TO_MIRROR_KEYS(Enum):
+    TEXT = 1
+
+
+# Callback for consuming incoming messages
+def consume_server_message(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+
+def init():
+    # Create a local messaging connection
+    Config = configparser.ConfigParser()
+    Config.read('./config/mirror_config.ini')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=Config.get('General', 'messaging_ip')))
+
+    global __channel
+    __channel = connection.channel()
+
+    # Create an exchange for the mirror-messages - type is direct so we can distinguish the different messages
+    __channel.exchange_declare(exchange='from-mirror', exchange_type='direct')
+    __channel.exchange_declare(exchange='to-mirror', exchange_type='direct')
+
+    # Declare a queue to be used (random name will be used)
+    result = __channel.queue_declare(exclusive=True)
+    queue_name = result.method.queue
+
+    # Listen to server messages
+    for msg_keys in MSG_TO_MIRROR_KEYS:
+        __channel.queue_bind(exchange='to-mirror',
+                           queue=queue_name,
+                           routing_key=msg_keys.name)
+
+    __channel.basic_consume(consume_server_message,
+                          queue=queue_name,
+                          no_ack=True)
+
+
+def start_consuming():
+    __channel.start_consuming()
+
+
+def send(key, body):
+    if __channel is None:
+        init()
+
+    if key not in MSG_FROM_MIRROR_KEYS.__members__:
+        print("[error] %r is not a valid message key to send to the server" % key)
+    else:
+        __channel.basic_publish(exchange='from-mirror',
+                          routing_key=key,
+                          body=body)
