9af843fbed7cbbef633a6e1586c884873143ba6d
Johanna Latt <j.latt@sap.com>
2018-05-25 13:58:27 -0400

    basic working skeleton rendering with kivy

--- rendering_widgets/gui_base_kivy.py
+++ rendering_widgets/gui_base_kivy.py
@@ -2,8 +2,8 @@ from kivy.config import Config
 
 Config.set('graphics', 'window_state', 'maximized')
 #Config.set('graphics', 'fullscreen', 'auto')
-Config.set('graphics', 'width', '1920')
-Config.set('graphics', 'height', '1080')
+Config.set('graphics', 'width', '1700')
+Config.set('graphics', 'height', '900')
 Config.set('graphics', 'resizable', 0)
 
 from kivy.app import App
@@ -14,32 +14,29 @@ from kivy.graphics import Line, Color, Rectangle, Ellipse
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 
-from rendering_widgets.skeleton_widget import SkeletonWidget
-
 from random import random as r
 from functools import partial
 import json
 
 
 # Define coordinate system that skeleton data arrives in
-min_x = -2000
+min_x = -3000
 min_y = -2000
-max_x = 2000
+max_x = 3000
 max_y = 2000
 
 
 class GUIBase(App):
 
     def render_skeleton_data(self, data_str):
-        self.skeleton_widget.setJointData(data_str)
-
-        '''self.skeleton_widget.canvas.clear()
+        self.skeleton_widget.canvas.clear()
 
         # Prepare the data
         data = json.loads(data_str)
 
         self.joint_data = data
 
+        # Convert the data to float
         for joint in self.joint_data:
             joint[0][0] = float(joint[0][0])
             joint[0][1] = float(joint[0][1])
@@ -48,28 +45,26 @@ class GUIBase(App):
             joint[1][1] = float(joint[1][1])
             joint[1][2] = float(joint[1][2])
 
-        print(self.joint_data)
+        sorted(self.joint_data, key=lambda x: x[0][2], reverse=True)
 
+        # Render the data - draw bones first and then the joints so they overlap the joints
         with self.skeleton_widget.canvas:
+            Color(0, 0, 1, mode='hsv')
             for joint in self.joint_data:
-                Color(0, 0, 1, mode='hsv')
-                Line(points=
-                        (self.rescale_joint_x_pos(joint[0][0]),
-                        self.rescale_joint_y_pos(joint[0][1]),
-                        self.rescale_joint_x_pos(joint[1][0]),
-                        self.rescale_joint_y_pos(joint[1][1])),
-                        width=7)
-                Color(0, 1, 1, mode='hsv')
-                self.draw_circle(self.rescale_joint_x_pos(joint[0][0]), self.rescale_joint_y_pos(joint[0][1]), 12)'''
+                from_x = self.rescale_joint_x_pos(joint[0][0])
+                from_y = self.rescale_joint_y_pos(joint[0][1])
+                to_x = self.rescale_joint_x_pos(joint[1][0])
+                to_y = self.rescale_joint_y_pos(joint[1][1])
+                Line(points=(from_x, from_y, to_x, to_y), width=3)
+
+            Color(0, 1, 1, mode='hsv')
+            for joint in self.joint_data:
+                from_x = self.rescale_joint_x_pos(joint[0][0])
+                from_y = self.rescale_joint_y_pos(joint[0][1])
+                self.draw_circle(from_x, from_y, 14)
 
     def clear_skeleton(self):
         self.skeleton_widget.canvas.clear()
-
-    def test(self):
-        with self.skeleton_widget.canvas:
-            Color(r(), 1, 1, mode='hsv')
-            self.draw_circle(150, 150, 30)
-            Line(points=(0, 0, 150, 150, 300, 100), width=3)
 
     def draw_circle(self, x, y, diameter):
         Ellipse(pos=(x - diameter / 2, y - diameter/2), size=(diameter,diameter))
@@ -83,11 +78,9 @@ class GUIBase(App):
     def build(self):
 
         # Fullscreen widget to draw the skeleton
-        self.skeleton_widget = SkeletonWidget()
+        self.skeleton_widget = Widget()
 
         root = FloatLayout()
         root.add_widget(self.skeleton_widget)
-
-        self.test()
 
         return root
